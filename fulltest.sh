cac8c82047ce99e9a43757da0caa27521288a3f8
Stephen Sugden <glurgle@gmail.com>
2011-10-11 23:52:30 -0700

    Fix up the fulltest.sh script

--- fulltest.sh
+++ fulltest.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 [[ -d lib-tmp ]] || mkdir lib-tmp
 coffee -o lib-tmp/ -c src/ && 
+cp src/fluid.js lib-tmp/
 rm -fr lib && 
 node-jscoverage lib-tmp/ lib/ &&
 vows test/*test* --spec --cover-html
+rm -fr lib && mv lib-tmp/ lib/

5565ddad4b2dd05120cf350c72d17c48be04b558
Stephen Sugden <glurgle@gmail.com>
2011-10-13 15:07:13 -0700

    Allow passing of test files to fulltest.sh

--- fulltest.sh
+++ fulltest.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+ARGS="$@"
+[[ -z "$@" ]] && ARGS='test/*test.coffee'
 [[ -d lib-tmp ]] || mkdir lib-tmp
 coffee -o lib-tmp/ -c src/ && 
 cp src/fluid.js lib-tmp/
 rm -fr lib && 
 node-jscoverage lib-tmp/ lib/ &&
-vows test/*test* --cover-html $@ && 
+vows --cover-html $ARGS && 
 rm -fr lib && mv lib-tmp/ lib/
